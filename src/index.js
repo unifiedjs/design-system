393f80cc997a9eb69356ec22a7e669131b898970
John Otander <johnotander@gmail.com>
2018-11-01 08:19:54 -0600

    Add box and flex for layouts

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ export { default as theme } from './theme'
 
 export { Heading } from './Heading'
 export { Text, Code } from './Text'
+
+export { Box } from './Box'
+export { Flex } from './Flex'
+
 export { Provider } from './Provider'
 
 export { default as mdComponents } from './mdComponents'
