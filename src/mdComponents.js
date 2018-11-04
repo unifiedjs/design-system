6d08947605fad81d7550136418289d25ac878190
John Otander <johnotander@gmail.com>
2018-11-03 22:13:46 -0600

    Tweak spacing for headings and text

--- src/mdComponents.js
+++ src/mdComponents.js
@@ -3,15 +3,15 @@ import React from 'react'
 import {Heading, Text, Code} from '.'
 
 export default {
-  h1: props => <Heading as="h1" mb={3} fontSize={5} {...props} />,
-  h2: props => <Heading as="h2" mb={3} fontSize={4} {...props} />,
-  h3: props => <Heading as="h3" mb={3} fontSize={3} {...props} />,
-  h4: props => <Heading as="h4" mb={3} fontSize={2} {...props} />,
-  h5: props => <Heading as="h5" mb={3} fontSize={1} {...props} />,
+  h1: props => <Heading as="h1" mt={4} mb={3} fontSize={5} {...props} />,
+  h2: props => <Heading as="h2" mt={4} mb={3} fontSize={4} {...props} />,
+  h3: props => <Heading as="h3" mt={4} mb={3} fontSize={3} {...props} />,
+  h4: props => <Heading as="h4" mt={4} mb={3} fontSize={2} {...props} />,
+  h5: props => <Heading as="h5" mt={4} mb={3} fontSize={1} {...props} />,
   h6: props => (
-    <Heading as="h6" mb={3} fontSize={1} color="grays.8" {...props} />
+    <Heading as="h6" mt={4} mb={3} fontSize={1} color="grays.8" {...props} />
   ),
-  p: props => <Text mb={3} fontSize={2} {...props} />,
+  p: props => <Text mt={4} mb={3} fontSize={2} {...props} />,
 
   li: props => <Text as="li" fontSize={2} {...props} />,
 
